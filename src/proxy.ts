6a2515f483926d3ac71c84767c662cbb391284e1
garzella190 <garzella190@gmail.com>
2026-03-15 01:47:18 +0100

    Update

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,8 @@
 import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+import { proxy } from '@/utils/supabase/proxy'
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  return await proxy(request)
 }
 
 export const config = {
